cffbcfc5303411fb165c2a9365dccd39168f3b25
platalytics <anas.sherwani@platalytics.com>
2016-03-20 15:41:05 +0100

    Added comments

--- src/main/scala/com/platalytics/platform/custom/Operation.scala
+++ src/main/scala/com/platalytics/platform/custom/Operation.scala
@@ -6,6 +6,15 @@ import org.apache.spark.SparkContext
 
 import scala.collection.mutable.{ Map => MutMap }
 
+/**
+* Interface to implement a custom operation
+* 
+* @param inputData    Map of all the ancestor stages along with their DataFrame
+* @param sparkContext
+* @param sqlContext
+* @param params       A mutable map containing all the process parameters (name, (value, type))
+* @return DataFrame   Resultant DataFrame created by performing the operation
+*/
 trait Operation {
   def run(inputData: Map[Int, DataFrame],
           sparkContext: SparkContext,
